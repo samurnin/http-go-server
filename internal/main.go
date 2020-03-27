9d333ddb9b4ab613b11daa93cf7b83c15b72fe96
Alexander Kosin <samurnin@gmail.com>
2020-03-27 14:17:23 -0400

    added implementation for REST API functions

--- internal/main.go
+++ internal/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/go-openapi/loads"
+	"github.com/go-openapi/runtime/middleware"
 	"github.com/samurnin/http-go-server/pkg/swagger/server/restapi"
 	"github.com/samurnin/http-go-server/pkg/swagger/server/restapi/operations"
 	"log"
@@ -13,13 +14,29 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	api := operations.NewHelloAPI(swaggerSpec)
 	server := restapi.NewServer(api)
-	defer server.Shutdown()
+	defer func() {
+		if err := server.Shutdown(); err != nil {
+			// error handle
+			log.Fatalln(err)
+		}
+	}()
 	server.Port = 8080
-	// Start listening using having the handlers and port
-	// already set up.
+
+	// Implement the CheckHealth handler
+	api.CheckHealthHandler = operations.CheckHealthHandlerFunc(
+		func(user operations.CheckHealthParams) middleware.Responder {
+			return operations.NewCheckHealthOK().WithPayload("OK")
+		})
+
+	// Implement the GetHelloUser handler
+	api.GetHelloUserHandler = operations.GetHelloUserHandlerFunc(
+		func(user operations.GetHelloUserParams) middleware.Responder {
+			return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User + "!")
+		})
+
+	// Start server which listening
 	if err := server.Serve(); err != nil {
 		log.Fatalln(err)
 	}
